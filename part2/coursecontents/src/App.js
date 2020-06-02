f3304e5a499c483a68d7116716a3cb86b139bb6e
Ville Lehtonen <violehtonen@gmail.com>
2020-06-02 21:21:54 +0200

    exercise 2.2

--- part2/coursecontents/src/App.js
+++ part2/coursecontents/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Course from './components/Course'
 
 const App = ({ course }) => {
+    let total = course.parts.map(course => course.exercises).reduce((a,b) => a+b, 0)
+
     return(
         <div>
             <h1> {course.name} </h1>
             <ul>
                 {course.parts.map(course => 
-                    <Course key = {course.id} course = {course} />
-                )}
+                    <Course key = {course.id} course = {course} />                )}
             </ul>
+                <b> Total of {total} exercises </b>
         </div>
     )}
 
