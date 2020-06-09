d70f4f80f30ffb3c681f11e175eb9bf82959b7a9
Ville Lehtonen <violehtonen@gmail.com>
2020-06-09 13:48:18 +0200

    exercise 3.7

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
+const requestLogger = (request, response, next) => {
+    console.log('Method: ', request.method)
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log("---")
+    next()
+}
+
+//app.use(requestLogger)
+//const unknownEndpoint = (request, response) => {response.status(404).send({ error: 'unknown endpoint'})}
+//app.use(unknownEndpoint)
 
 let persons = [
     { 
